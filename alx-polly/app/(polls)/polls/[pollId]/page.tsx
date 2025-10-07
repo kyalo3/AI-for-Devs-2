7a41f6e46f200bcea1d815388c7e1b6caa8ccc7e
kyalo3 <kyalokimeu0@gmail.com>
2025-10-07 11:06:31 +0300

    deleted the sample files & reinstalled the alx-polly folders

--- alx-polly/app/(polls)/polls/[pollId]/page.tsx
+++
@@ -1,5 +0,0 @@
-import { PollView } from "@/components/polls/poll-view";
-
-export default function PollPage({ params }: { params: { pollId: string } }) {
-  return <PollView pollId={params.pollId} />;
-}
